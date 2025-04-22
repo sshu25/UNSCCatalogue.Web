a4dcd397bb6744c7f07201057cafb95255b90490
stephen.hu <stephen.hu25@gmail.com>
2025-04-22 13:52:45 -0400

    Added searchbar to products

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -10,24 +10,18 @@ namespace UNSCCatalogue.Web.Controllers
 {
     public class ProductsController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string search = "")
         {
             UNSCdbEntities db = new UNSCdbEntities();
-            List<Product> products = db.Products.ToList();
+            List<Product> products = db.Products.Where(x => x.Name.Contains(search)).ToList();
+            ViewBag.Search = search;
             return View(products);
         }
 
         //public ActionResult Details(int id)
         //{
-        //    Product prod = null;
-        //    foreach (var product in products)
-        //    {
-        //        if (product.ID == id)
-        //        {
-        //            prod = product;
-        //        }
-        //    }
-
+        //    UNSCdbEntities db = new UNSCdbEntities();
+        //    Product prod = db.Products.Where(x => x.ID == id);
         //    return View(prod);
         //}
 
