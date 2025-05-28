71a89d4823cadec72e4e323d8f26450da3ac358b
stephen.hu <stephen.hu25@gmail.com>
2025-05-28 01:02:05 -0400

    WIP Create account

---
+++ ViewModels/Create.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UNSCCatalogue.Web.ViewModels
+{
+    public class Create
+    {
+        [Required]
+        public string Username {  get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Must be same as Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        public string Mobile { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+    }
+}
