d89c3e71e0e413f74d4a3cddc1d5d1bf0a1c93de
stephen.hu <stephen.hu25@gmail.com>
2025-06-07 19:59:56 -0400

    Added Users page for Admin to show all Users

---
+++ Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UNSCCatalogue.Web.Identity;
+using UNSCCatalogue.Web.Models;
+
+namespace UNSCCatalogue.Web.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Admin/Users
+        public ActionResult Index()
+        {
+            IdentitydbContext db = new IdentitydbContext();
+            List<User> users = db.Users.ToList();
+            return View(users);
+        }
+    }
+}
